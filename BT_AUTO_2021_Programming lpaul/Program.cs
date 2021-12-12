760900815cf2e10681939de2fc4b669f155b1ed2
Lică Paul <lica.paul11@gmail.com>
2021-12-12 22:48:29 +0200

    Add project files.

---
+++ BT_AUTO_2021_Programming lpaul/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BT_AUTO_2021_Programming_lpaul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
