f6c8b487b450b5d2bbbf51d7a9b2faa5fee3e638
Lică Paul <lica.paul11@gmail.com>
2021-12-12 23:31:38 +0200

    updates on the program with more Console writes

--- BT_AUTO_2021_Programming lpaul/Program.cs
+++ BT_AUTO_2021_Programming lpaul/Program.cs
@@ -7,6 +7,8 @@ namespace BT_AUTO_2021_Programming_lpaul
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(6 + 2);
+            Console.WriteLine("6 + 2");
         }
     }
 }
